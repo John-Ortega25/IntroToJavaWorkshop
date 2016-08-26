bbb4cd449ebe0b5860017346917b0e9b48f2c055
John-Ortega25 <league@Mac-mini-2.local>
2016-08-26 11:56:52 -0700

    Day 5

--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -19,28 +19,41 @@ public class FlamingNinjaStar {
 		// 1. Make a new robot, and set it's pen down.
 
 		// 12. Set the robot speed to 10
-
+		Robot John= new Robot(); 
+		John.penDown();
+		John.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
-
-			// 2. Turn the robot 1/8 of a circle
+		for (int i = 0; i < 25; i++) {
+			
 		
+			// 2. Turn the robot 1/8 of a circle
+		John.turn(360/8);
 			// 3. Move the robot 64 pixels
-
+		John.move(64);
+		John.penUp();
+		John.setPenColor(170,017,034);
+		John.penDown();
 			
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+		John.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+		John.move(flameSize);	
 			// 6. Turn the robot 170 degrees
-			
+		John.turn(170);	
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+		John.move(flameSize);	
 			// 8. Turn the robot 64 degrees to the right
-			
+		John.turn(64);	
 			// 9. Move the robot the distance in the variable baseSize
+		John.move(baseSize);	
 			
-			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
-			// 11. Color your ninja star like Figure 2.
+		
+		
+		
+		}
+		// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+		
+		// 11. Color your ninja star like Figure 2.
 		
 	}
 
--- src/day5/StarShow.java
+++ src/day5/StarShow.java
@@ -15,44 +15,52 @@ public class StarShow {
 		
 		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+		robot.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+		int X=(10);
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+		int Y=(950);
 		// 8. Make a variable to hold the star size and set it to 25
-
+		int StarSize=(25);
 		// 12. Repeat the steps #19 to #18, 30 times
-
+		for (int i = 0; i < 30; i++) {
+			robot.setRandomPenColor();
+			robot.setPenWidth(i);
+		
 			// 19. Set the pen width to i 
 	
 			// 10. Set the X position of the robot to your X variable
-	
+		robot.setX(X);
 			// 11. Set the Y position of the robot to your Y variable
-	
+		robot.setY(Y);
 			// 9. Call the drawStar() method with your star size variable
-	
+		drawStar(StarSize);
 			// 14. Increase the X position by star size. See Figure 2.
-	
-			// 15. decrease the Y position by star size. See Figure 3.
-	
+		robot.setX(X+StarSize);
+				// 15. decrease the Y position by star size. See Figure 3.
+		robot.setY(Y=StarSize);
 			// 16. Increase the star size by 20
-	
+		
 			// 17. Turn the robot 12 degrees
 	
 			// 18. Make each star a different random color like in Figure 4. 
-
+		}
 	}
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+		robot.setSpeed(10);
+		robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-
-			// 1. Move the robot the distance of the starSize variable
-	
-			// 3. Turn the robot 144 degrees
+		for (int i = 0; i < 5; i++) {
 			
+			robot.move(starSize);
+			robot.turn(144);
+		}
+			// 1. Move the robot the distance of the starSize variable
+		robot.move(starSize);
+			// 3. Turn the robot 144 degrees
+			robot.turn(144);
 	}
 	
 	public static void main(String[] args) {

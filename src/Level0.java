41d85c1fe7b9bd1ffc18c25e06558d216629b412
John-Ortega25 <league@Mac-mini-3.local>
2016-10-08 11:33:17 -0700

    10-8-16
    Level 0 first day

---
+++ src/Level0.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Level0 {
+	public static void main(String[] args) {
+		int numTimes= 1;
+		while(numTimes<= 4){
+			numTimes+=1;
+		JOptionPane.showMessageDialog(null, "Whats a joke?");
+		JOptionPane.showMessageDialog(null, "I don't know");
+	}
+	
+}
+}
---
+++ src/while_loop_challenge.java
@@ -0,0 +1,11 @@
+
+public class while_loop_challenge {
+public static void main(String[] args) {
+	int number= -8;
+	while(number<-4){
+	number=number+1;
+		System.out.print(number);
+	
+	}
+}
+}

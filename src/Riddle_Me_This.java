80817de40a8ab074df11ef900b2753d69d594cd9
Savera <Level 2>
2016-10-22 11:32:52 -0700

    papi
    papipapipapi

---
+++ src/Riddle_Me_This.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Riddle_Me_This {
+public static void main(String[] args) {
+	String input= JOptionPane.showInputDialog(null, "Whats your name");
+	String name= "John";
+	int numRight= 0;
+	
+	if (name.equals(input)) {
+		
+		numRight=numRight+1;
+	 	
+	}
+	JOptionPane.showMessageDialog(null, "you got " +numRight+ " right");	
+}
+}

dd63d83e93c576af0ff6d7a2a3aca24092815550
John-Ortega25 <league@Mac-mini-2.local>
2016-11-05 11:37:22 -0700

    For loops

---
+++ src/Years_Alive.java
@@ -0,0 +1,16 @@
+
+public class Years_Alive {
+	public static void main(String[] args) {
+		int age = 0;
+		while (age < 15) {
+			System.out.println(age);
+			age = age + 1;
+
+		}
+
+		for (int x = 0; x <= 14; x = x + 1) {
+			System.out.println(x);
+		}
+
+	}
+}
---
+++ src/for_loop_challenge.java
@@ -0,0 +1,12 @@
+
+public class for_loop_challenge {
+	public static void main(String[] args) {
+		for (int num = 2; num < 12; num = num + 2) {
+			System.out.println(num);
+		}
+		for (int Num = 99; Num > 93; Num = Num - 1) {
+			System.out.println("New age "+Num);
+
+		}
+	}
+}
